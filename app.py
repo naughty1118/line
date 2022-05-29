124e8ad51527c0205fdbbc5a10a08cfd3ee75a34
naughty1118 <106423560+naughty1118@users.noreply.github.com>
2022-05-29 19:05:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -49,5 +49,5 @@ def handle_message(event):
 #主程式
 import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 8787))
     app.run(host='0.0.0.0', port=port)
